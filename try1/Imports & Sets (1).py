7c25707deae7347f164c00d3ffc9155a0c44c85a
Ori Korngut <ori.korngut@gmail.com>
2023-06-01 19:29:51 +0300

    Start

--- try1/Imports & Sets (1).py
+++ try1/Imports & Sets (1).py
@@ -1,18 +1,20 @@
-#----------------------------------------------- SoftWare Updates / Imports --------------------------------------------------
-from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
-from spike.control import wait_for_seconds, wait_until, Timer
-from math import *
-import time
-#----------------------------------------------- Motor / Variable Declaration--------------------------------------------------
-hub = PrimeHub()
-motora = Motor('A')
-motorb = Motor('B')
-motorc = Motor('C')
-motord = Motor('D')
-pairAB = MotorPair('A','B')
-hub.motion_sensor.reset_yaw_angle()
-motorc.set_degrees_counted(0)
-perror = 0
-I = 0
-x = hub.motion_sensor.get_yaw_angle()
-print("www")
+
+# Load the image
+img = imread('Downloads\\dolphin.png')
+
+# Display the original image
+imshow('Original Image', img)
+waitKey(0)
+
+# Crop the image
+cropped = img[110:310, 10:160]
+
+# Display the cropped image
+imshow('Cropped Image', cropped)
+waitKey(0)
+
+# Find the size of the cropped image
+cropped_height, cropped_width, _ = cropped.shape
+print('Cropped image size:', cropped_width, 'x', cropped_height)
+destroyAllWindows()
+
